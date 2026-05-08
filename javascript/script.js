19fdfb32133a702df5a3e04d6099dd9b2fdc6479
Lucas <lucag83@nmsu.edu>
2026-05-08 17:27:23 -0600

    javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,30 @@
-function showAlert() {
-    alert("Thank you for your interest in a more sustainable future! Please take a look at the products we offer.")
-}
+document.addEventListener('DOMContentLoaded', () => {
 
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
 
-if(menuToggle) {
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        console.log("Menu formated for mobile viewing");
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (menuToggle && navLinks) {
+        menuToggle.addEventListener('click', () => {
+         
+            navLinks.classList.toggle('active');
+            
+            
+            console.log("Mobile menu state toggled.");
+        });
+    }
+
+
+    const navItems = document.querySelectorAll('.nav-links a');
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+        });
     });
+});
+
+
+function showAlert() {
+    
+    alert("Thank you for your interest in a more sustainable future! Please take a look at the products we offer.");
 }
